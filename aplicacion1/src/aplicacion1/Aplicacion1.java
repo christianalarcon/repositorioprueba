4b58e56a01eda77d22a8089f09ee4d8d7d48da89
christianalarcon <christian.alarcon13@gmail.com>
2015-10-27 15:37:01 +0100

    Update Aplicacion1.java

--- aplicacion1/src/aplicacion1/Aplicacion1.java
+++ aplicacion1/src/aplicacion1/Aplicacion1.java
@@ -17,6 +17,9 @@ public class Aplicacion1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        
+        
+        System.out.println("hola 2");
     }
     
 }
